8c0f69a4a51d0923a4c1f370de66fc90bf97ffd6
lyric <tiannianshou@gmail.com>
2018-04-27 17:47:12 +0800

    update test

--- hook_test.go
+++ hook_test.go
@@ -13,7 +13,7 @@ import (
 )
 
 func TestHook(t *testing.T) {
-	db, err := sql.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/test?charset=utf8")
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/myapp_test?charset=utf8")
 	if err != nil {
 		t.Error(err)
 		return
@@ -87,7 +87,7 @@ func TestHook(t *testing.T) {
 }
 
 func ExampleHook() {
-	db, err := sql.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/test?charset=utf8")
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/myapp_test?charset=utf8")
 	if err != nil {
 		fmt.Println(err)
 		return
