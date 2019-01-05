02928fefa745679e76c498c0ca877801d0e16680
lyric <tiannianshou@gmail.com>
2019-01-05 21:05:15 +0800

    Fixed exec

--- exec.go
+++ exec.go
@@ -38,7 +38,7 @@ func NewExec(db *sql.DB, tableName string, extraItems ...*ExecExtraItem) Execer 
 		sourceItems = append(sourceItems, extraItems...)
 	}
 	sourceItems = append(sourceItems, NewExecExtraItem("data", "text"))
-	sourceItems = append(sourceItems, NewExecExtraItem("created", "DATETIME"))
+	sourceItems = append(sourceItems, NewExecExtraItem("created_at", "DATETIME"))
 
 	var fields []string
 	for _, item := range sourceItems {
@@ -87,7 +87,7 @@ func (e *defaultExec) Exec(entry *logrus.Entry) error {
 				data = string(jsonData)
 			}
 			values = append(values, data)
-		case "created":
+		case "created_at":
 			values = append(values, entry.Time)
 		default:
 			var value interface{}
